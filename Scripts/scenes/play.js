a31db97040d0c2d4babd57b1172e94d6d518c758
Michael Tran <minhtran1995@hotmail.com>
2016-03-29 01:40:13 -0400

    Comment Added

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -3,11 +3,12 @@
  * Source file name : play.ts                                                          *
  * Author's name : Duc Minh Tran (300771859)                                           *
  * Last Modified by : Duc Minh Tran (300771859)                                        *
- * Last Modified date : March 27 2016                                                  *
+ * Last Modified date : March 29 2016                                                  *
  * Program description : This is a webgame that use  a Side Scroller background        *
  *                                                                                     *
  * Revision History : 1 - Update Internal Documentation                                *
  *                    2 - Change Winning Score                                         *
+ *                    3 - Adding Comments                                              *
  *                                                                                     *
  ***************************************************************************************
 */
@@ -28,36 +29,46 @@ var scenes;
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
         Play.prototype.start = function () {
+            //init static variable
             Play._counter = 0;
-            this._captainShieldCount = 10;
-            this._healthCount = 1;
-            this._captainShields = new Array();
-            this._health = new Array();
+            //adding scrolling background
             this._city = new objects.City();
             this.addChild(this._city);
             this._setupBackground('blank');
+            //health count
+            this._health = new Array();
+            this._healthCount = 1;
             for (var h = 0; h < this._healthCount; h++) {
                 this._health[h] = new objects.Health();
                 this.addChild(this._health[h]);
             }
+            //player object
             this._player = new objects.Player();
             this._bullet = new objects.Bullet(this._player);
             this.addChild(this._bullet);
             this.addChild(this._player);
+            //adding captain shields
+            this._captainShieldCount = 10; //number of shields
+            this._captainShields = new Array();
             for (var shield = 0; shield < this._captainShieldCount; shield++) {
                 this._captainShields[shield] = new objects.CaptainShield();
                 this.addChild(this._captainShields[shield]);
             }
+            //init collision manager
             this._collision = new managers.Collision(this._player, this);
+            //score label
             this.point = 0;
             this._score = new objects.Label("Score: ", "30px Orbitron", "#adffff", 10, 0, false);
             this.addChild(this._score);
+            //health label
             this.health = 100;
             this._healthLabel = new objects.Label("%", "35px Orbitron", "#adffff", config.Screen.WIDTH - 230, 0, false);
             this.addChild(this._healthLabel);
+            //dead message
             this._deadLabel = new objects.Label("You are Dead !", "Bold 50px Orbitron", "#ff1a1a", config.Screen.CENTER_X, config.Screen.CENTER_Y, true);
             this._deadLabel.visible = false;
             this.addChild(this._deadLabel);
+            //adding health symbol
             this.healthIMG = new createjs.Bitmap(assets.getResult("health"));
             this.healthIMG.x = config.Screen.WIDTH - this.healthIMG.getBounds().width * 0.5;
             this.healthIMG.y = this.healthIMG.getBounds().height * 0.5;
@@ -79,12 +90,15 @@ var scenes;
             else {
                 this._bullet.reset(config.Screen.WIDTH + (this._bullet.width) * 5);
             }
+            //update player location
             this._player.update();
+            //update shields locations and check collision
             this._captainShields.forEach(function (shield) {
                 shield.update();
                 _this._collision.check(shield);
                 _this._collision.bulletCollision(_this._bullet, shield);
             });
+            //update health locations and check collision
             this._health.forEach(function (h) {
                 h.update();
                 _this._collision.check(h);
--- Scripts/scenes/play.ts
+++ Scripts/scenes/play.ts
@@ -3,11 +3,12 @@
  * Source file name : play.ts                                                          *
  * Author's name : Duc Minh Tran (300771859)                                           *
  * Last Modified by : Duc Minh Tran (300771859)                                        *
- * Last Modified date : March 27 2016                                                  *
+ * Last Modified date : March 29 2016                                                  *
  * Program description : This is a webgame that use  a Side Scroller background        * 
  *                                                                                     *  
  * Revision History : 1 - Update Internal Documentation                                *
  *                    2 - Change Winning Score                                         *
+ *                    3 - Adding Comments                                              *
  *                                                                                     *
  ***************************************************************************************
 */
@@ -22,10 +23,12 @@ module scenes {
         private _captainShieldCount: number;
         private _healthCount: number;
 
+        //game objects
         private _bullet: objects.Bullet;
         private _player: objects.Player;
         private _collision: managers.Collision;
 
+        //score and health
         private _score: objects.Label;
         public point: number;
         public health: number;
@@ -33,7 +36,7 @@ module scenes {
 
         public healthIMG: createjs.Bitmap;
 
-
+        //game labels
         private _healthLabel: objects.Label;
         private _deadLabel: objects.Label;
 
@@ -49,58 +52,65 @@ module scenes {
 
         // Start Method
         public start(): void {
+            //init static variable
             Play._counter = 0;
-            this._captainShieldCount = 10;
-            this._healthCount = 1;
-            this._captainShields = new Array<objects.CaptainShield>();
-            this._health = new Array<objects.Health>();
 
+            //adding scrolling background
             this._city = new objects.City();
             this.addChild(this._city);
 
             this._setupBackground('blank');
 
-
+            //health count
+            this._health = new Array<objects.Health>();
+            this._healthCount = 1;
             for (var h = 0; h < this._healthCount; h++) {
                 this._health[h] = new objects.Health();
                 this.addChild(this._health[h]);
             }
 
-
-
-
+            //player object
             this._player = new objects.Player();
             this._bullet = new objects.Bullet(this._player);
             this.addChild(this._bullet);
             this.addChild(this._player);
+
+            //adding captain shields
+            this._captainShieldCount = 10;//number of shields
+            this._captainShields = new Array<objects.CaptainShield>();
+
 
             for (var shield = 0; shield < this._captainShieldCount; shield++) {
                 this._captainShields[shield] = new objects.CaptainShield();
                 this.addChild(this._captainShields[shield]);
             }
 
-
+            //init collision manager
             this._collision = new managers.Collision(this._player, this);
 
+            //score label
             this.point = 0;
             this._score = new objects.Label("Score: ", "30px Orbitron",
                 "#adffff",
                 10, 0, false);
             this.addChild(this._score);
 
-
+            //health label
             this.health = 100;
             this._healthLabel = new objects.Label("%", "35px Orbitron",
                 "#adffff",
                 config.Screen.WIDTH - 230, 0, false);
             this.addChild(this._healthLabel);
 
+
+            //dead message
             this._deadLabel = new objects.Label("You are Dead !", "Bold 50px Orbitron",
                 "#ff1a1a",
                 config.Screen.CENTER_X, config.Screen.CENTER_Y, true);
             this._deadLabel.visible = false;
             this.addChild(this._deadLabel);
 
+            //adding health symbol
             this.healthIMG = new createjs.Bitmap(assets.getResult("health"));
             this.healthIMG.x = config.Screen.WIDTH - this.healthIMG.getBounds().width * 0.5;
             this.healthIMG.y = this.healthIMG.getBounds().height * 0.5;
@@ -125,16 +135,17 @@ module scenes {
                 this._bullet.reset(config.Screen.WIDTH + (this._bullet.width) * 5);
             }
 
+            //update player location
             this._player.update();
 
-
+            //update shields locations and check collision
             this._captainShields.forEach(shield => {
                 shield.update();
                 this._collision.check(shield);
                 this._collision.bulletCollision(this._bullet, shield);
             });
 
-
+            //update health locations and check collision
             this._health.forEach(h => {
                 h.update();
                 this._collision.check(h);
@@ -179,7 +190,7 @@ module scenes {
                 window.onmousedown = function() {
                     console.log("Mouse disabled");
                 };
-                
+
                 if (Play._counter === 180) {
                     this._fadeOut(500, () => {
                         // Switch to the win Scene                
@@ -192,9 +203,6 @@ module scenes {
             }
 
         }
-
-
-        //EVENT HANDLERS ++++++++++++++++++++
 
     }
 }
